473303140a0d6922f01b3b3d2aed92bf46030f07
Nick Apostolakis <nickapos@oncrete.gr>
2017-09-17 08:41:03 +0100

    refactoring functions, adding browser and options variables. adding delay, replacing while with for and sequence

--- browser.sh
+++ browser.sh
@@ -1,31 +1,32 @@
 #!/bin/sh
 
+#BROWSER='wget'
+#BROWSER_OPTIONS='-O /dev/null -o /dev/null'
+
+BROWSER='firefox'
+BROWSER_OPTIONS='-headless'
+DELAY=5
+SITE='http://google.com'
+
 makeAndKillBrowsers() {
-  a=0
-
-  while [ $a -lt 1 ]
-  do
-     #echo $a
-     a=`expr $a + 1`
-     echo "opening browser $a"
-     lynx  http://google.com
-
-     getAndKillRunningBrowserPids
-  done
-  echo "done"
+  $BROWSER $BROWSER_OPTIONS $SITE &
+  echo "waiting $DELAY seconds"
+  sleep $DELAY
+  getAndKillRunningBrowserPids
 }
 
 getAndKillRunningBrowserPids() {
-  echo "preparing to kill all browser processes"
-  pids=$(pgrep lynx)
-  echo $pids
-  for i in $pids
+  echo "preparing to kill all browser $BROWSER processes"
+  for i in $(pgrep $BROWSER)
   do
     echo "killing now process $i....."
     kill -9 $i
-
   done
 }
 
-#getAndKillRunningBrowserPids
-makeAndKillBrowsers
+for i in $(seq 1 10)
+do
+  echo "opening browser $i"
+  makeAndKillBrowsers
+done
+echo "done"
